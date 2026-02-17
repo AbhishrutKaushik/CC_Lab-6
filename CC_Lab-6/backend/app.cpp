614f7c0af6afe12dcf7b67488f7aa18b028745aa
AbhishrutKaushik <abhishrutkaushik13@gmail.com>
2026-02-17 16:50:19 +0530

    Fix HTTP response handling with Content-Length header

--- CC_Lab-6/backend/app.cpp
+++ CC_Lab-6/backend/app.cpp
@@ -42,13 +42,20 @@ int main() {
         int client_fd = accept(server_fd, NULL, NULL);
         if (client_fd < 0) continue;
         
+        // Read the request first (consume it)
+        char buffer[1024];
+        read(client_fd, buffer, sizeof(buffer));
+        
         // Simple HTTP response
+        std::string body = "Served by backend: " + std::string(hostname) + "\n";
         std::string response = "HTTP/1.1 200 OK\r\n";
         response += "Content-Type: text/plain\r\n";
+        response += "Content-Length: " + std::to_string(body.length()) + "\r\n";
         response += "Connection: close\r\n\r\n";
-        response += "Served by backend: " + std::string(hostname) + "\n";
+        response += body;
         
         send(client_fd, response.c_str(), response.length(), 0);
+        shutdown(client_fd, SHUT_WR);
         close(client_fd);
     }
     
